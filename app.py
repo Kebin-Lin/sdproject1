309812b10dbe93363457f0c61a31b25816fc6c01
Sajed Nahian <snahian@stuy.edu>
2018-11-27 12:39:51 -0500

    added more login functionality

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-from flask import Flask, render_template, request, session, url_for, redirect
-import os,random
+from flask import Flask, render_template, request, session, url_for, redirect, flash
+import os
 
 from util import apihelp as api
 
@@ -16,30 +16,19 @@ def debugPrint(toPrint):
 
 @app.route("/", methods = ["POST", "GET"])
 def input_field_page():
-	#session.clear()
+	# session.clear()
 	if "username" in session:
 		debugPrint("logged in as " + session["username"])
 	#print(api.getOMDbURL('Kung Fury', 1))
 	return render_template('homepage.html')
 
-@app.route("/profile",methods = ["POST", "GET"])
-def profile():
-	#test movielist
-	names=["The Dark Knight","Deadpool", "Avengers","The Crow"]
-	ml={}
-	for name in names:
-		ml[name]=api.getOMDBdata(name)
-	recm={}
-	recommendations=api.getTasteDiveData(names)
-	testmovie=recommendations[random.randint(0,9)]["Name"]
-	recm=api.getOMDBdata(testmovie)
-	return render_template("profile.html",user="me", movielist=ml,recmovie=recm,)
-
 @app.route("/auth", methods=["POST"])
 def auth_account():
 	if _temp_login(request.form["username"], request.form["password"]):
+		debugPrint("Successful Login")
 		session["username"] = request.form["username"]
 	else:
+		debugPrint("Failed Login")
 		flash("Invalid Login Credentials")
 	return redirect(url_for("input_field_page"))
 
@@ -52,15 +41,19 @@ def sign_up_page ():
 @app.route("/createaccount", methods=["POST"])
 def create_account():
 	if (request.form['password'] == request.form['passwordConfirmation']):
-		if not(True):
+		if fakeCheckIfUserInDB(request.form['username']):
 			flash("Username already taken")
-			return redirect(url_for("sign_up_page"))
+			return render_template('homepage.html')
 		else:
 			flash("Account created successfully")
 			return redirect(url_for("input_field_page"))
 	else:
 		flash("Password do not match")
-		return redirect(url_for("sign_up_page"))
+	return redirect(url_for("sign_up_page"))
+
+
+def fakeCheckIfUserInDB(username):
+	return username == "test"
 
 def _temp_login(username, password):
 	return username == hardcodedUser["username"] and password == hardcodedUser["password"]
@@ -71,3 +64,4 @@ if __name__ == "__main__":
 # API INFO:
 	# TasteDive: 324021-MyNextMo-WHLW4A5Z
 # our email: mynextmovieapp@gmail.com password: stuysoftdev1
+
